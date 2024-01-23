0e8f711db72a742e5ebf565efa6c42bfb3a6000b
moby1804 <mogbitseo@yahoo.com>
2024-01-23 03:27:57 +0000

    added email form

---
+++ lib/email_form_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // ignore: prefer_const_constructors
+            Image.asset(
+              'assets/images/webminds logo.png',
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  TextField(
+                    style: TextStyle(color: Colors.blueGrey),
+                    decoration: InputDecoration(
+                      hintText: 'Enter email',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      filled: true,
+                      fillColor: Color.fromRGBO(230, 245, 238, 1),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Sign In'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
